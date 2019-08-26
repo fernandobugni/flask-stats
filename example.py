6fc255862403ddc211d0f4e8e4baee117bc609c6
Gaturron <fernando.bugni@gmail.com>
2019-08-25 23:42:15 -0300

    Removing unused comments

--- example.py
+++ example.py
@@ -1,16 +1,12 @@
 
 from time import sleep
-
 from flask import Flask
-
-from flask_stats.time_decorator import time_decorator
 from flask_stats.flask_stats import Stats
 
 app = Flask(__name__)
 Stats(app)
 
 @app.route('/')
-#@time_decorator
 def hello():
     a = 40
     sleep(10)
@@ -20,27 +16,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
-
-    # In order to run a function that is subscripted by a route
-    # you have to do: app.view_functions['hello']()
-
-    # To search for the endpoint method could be
-    # [x for x in app.url_map.iter_rules()]
-
-    # To add code by metaprogramming
-    # from functools import wraps
-    # def debug(func):
-    #     '''decorator for debugging passed function'''
-    #     @wraps(func)
-    #     def wrapper(*args, **kwargs):
-    #         print("Full name of this method:", func.__qualname__)
-    #         return func(*args, **kwargs)
-    #     return wrapper
-    # def f():
-    #     print("111")
-    #
-    # debug(f)
-    # <function f at 0x7f5e1db86510>
-    # debug(f)()
-    # Full name of this method: f
-    # 111
